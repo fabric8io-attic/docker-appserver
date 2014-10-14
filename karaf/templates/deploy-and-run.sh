af8d9b6e810ed08d0257dd0d6119c52031ef4ab3
Ioannis Canellos <iocanel@gmail.com>
2014-10-14 11:52:18 +0300

    Allow karaf to handle both kars and offline repository

--- karaf/templates/deploy-and-run.sh
+++ karaf/templates/deploy-and-run.sh
@@ -2,9 +2,11 @@
 
 
 DIR=${DEPLOY_DIR:-/maven}
-echo "Checking in $DIR"
+echo "Copying offline repo"
+cp -rf $DIR/repository/* /opt/karaf/system/
+echo "Checking for kars in $DIR/kars"
 if [ -d $DIR ]; then
-  for i in $DIR/*; do
+  for i in $DIR/kars/*; do
      file=$(basename $i)
      echo "Linking $i --> /opt/karaf/deploy/$file"
      ln -s $i /opt/karaf/deploy/$file
