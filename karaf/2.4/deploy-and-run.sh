f38c8ab73afc95541e118758227753aad6226cf0
Jimmi Dyson <jimmidyson@gmail.com>
2014-11-03 14:40:01 +0000

    Add onbuild to be able to use straight features as well as kars

--- karaf/2.4/deploy-and-run.sh
+++ karaf/2.4/deploy-and-run.sh
@@ -2,16 +2,26 @@
 
 
 DIR=${DEPLOY_DIR:-/maven}
-echo "Copying offline repo"
-cp -rf $DIR/repository/* /opt/karaf/system/
-echo "Checking for kars in $DIR/kars"
-if [ -d $DIR ]; then
+
+if [ -d "$DIR/repository" ]; then
+  echo "Copying offline repo"
+  cp -rf $DIR/repository/* /opt/karaf/system/
+fi
+
+if [ -d "$DIR/kars" ]; then
   for i in $DIR/kars/*; do
      file=$(basename $i)
      echo "Linking $i --> /opt/karaf/deploy/$file"
      ln -s $i /opt/karaf/deploy/$file
   done
 fi
-# Use faster (though more unsecure) random number generator
-export KARAF_OPTS="$KARAF_OPTS -Djava.security.egd=file:/dev/./urandom"
-/opt/karaf/bin/karaf server
+
+KARAF_USERNAME=${KARAF_USERNAME:-admin}
+KARAF_PASSWORD=${KARAF_PASSWORD:-admin}
+
+echo ${KARAF_USERNAME}=${KARAF_PASSWORD},_g_\:admingroup > /opt/karaf/etc/users.properties
+echo '_g_\:admingroup = group,admin,manager,viewer,webconsole' >> /opt/karaf/etc/users.properties
+
+export KARAF_OPTS="-javaagent:/opt/jolokia/jolokia.jar=host=0.0.0.0,port=8778,authMode=jaas,realm=karaf,user=${KARAF_USERNAME},password=${KARAF_PASSWORD},agentId=$HOSTNAME"
+
+exec /opt/karaf/bin/karaf server
