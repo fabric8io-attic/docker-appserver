824fa3638bee2f1de941f851cfe20190daf85e37
James Strachan <james.strachan@gmail.com>
2014-11-13 20:15:45 +0000

    regenerated containers and deleting the ROOT folder in tomcat

--- karaf/2.4/deploy-and-run.sh
+++ karaf/2.4/deploy-and-run.sh
@@ -2,26 +2,16 @@
 
 
 DIR=${DEPLOY_DIR:-/maven}
-
-if [ -d "$DIR/repository" ]; then
-  echo "Copying offline repo"
-  cp -rf $DIR/repository/* /opt/karaf/system/
-fi
-
-if [ -d "$DIR/kars" ]; then
+echo "Copying offline repo"
+cp -rf $DIR/repository/* /opt/karaf/system/
+echo "Checking for kars in $DIR/kars"
+if [ -d $DIR ]; then
   for i in $DIR/kars/*; do
      file=$(basename $i)
      echo "Linking $i --> /opt/karaf/deploy/$file"
      ln -s $i /opt/karaf/deploy/$file
   done
 fi
-
-KARAF_USERNAME=${KARAF_USERNAME:-admin}
-KARAF_PASSWORD=${KARAF_PASSWORD:-admin}
-
-echo ${KARAF_USERNAME}=${KARAF_PASSWORD},_g_\:admingroup > /opt/karaf/etc/users.properties
-echo '_g_\:admingroup = group,admin,manager,viewer,webconsole' >> /opt/karaf/etc/users.properties
-
-export KARAF_OPTS="-javaagent:/opt/jolokia/jolokia.jar=host=0.0.0.0,port=8778,authMode=jaas,realm=karaf,user=${KARAF_USERNAME},password=${KARAF_PASSWORD},agentId=$HOSTNAME"
-
-exec /opt/karaf/bin/karaf server
+# Use faster (though more unsecure) random number generator
+export KARAF_OPTS="$KARAF_OPTS -Djava.security.egd=file:/dev/./urandom"
+/opt/karaf/bin/karaf server
